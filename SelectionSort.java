0713e032a690cf4ce6b396472e669d0c27800ece
Hanif Mirza <hanif0088@gmail.com>
2016-10-03 12:19:43 -0400

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,35 @@
+public class SelectionSort 
+{
+	public static void main(String[] args) 
+	{
+		int[] myArray = {1,23,45,11,9,7,67,99};
+		myArray = doSelectionSort(myArray);
+		
+		for(int i = 0; i<myArray.length; i++)
+		{
+			System.out.println(myArray[i]);
+		}
+	}
+	
+	//Time complexity O(n^2) and space complexity O(1)
+	public static int[] doSelectionSort(int[] array)
+	{
+		for(int i = 0; i < array.length - 1; i++)
+		{
+			// Find the index of the minimum value
+			int minPosition = i;
+			for(int j= i+1; j < array.length; j++)
+			{
+				if( array[j] < array[minPosition])
+				{
+					minPosition = j;
+				}
+				//swapping the two values 
+				int smallerNumber = array[minPosition];
+				array[minPosition] = array[i];
+				array[i] = smallerNumber;
+			}
+		}
+		return array;
+	}
+}
