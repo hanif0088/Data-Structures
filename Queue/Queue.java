fc1f939d395839f65057cf45efea929ff12ce6f9
Hanif Mirza <hanif0088@gmail.com>
2016-10-22 00:24:21 -0400

    Create Queue.java

---
+++ Queue/Queue.java
@@ -0,0 +1,107 @@
+/*
+ * COMP 2270 Data Structure
+ * Java Program to Implement Queue
+ * front<=========<=rear<=
+ */
+
+class Queue
+{
+    int[] queue ;
+    int front, rear, maxSize, count;
+
+    public Queue(int size)
+    {
+        maxSize = size;
+        count = 0;
+        queue = new int[maxSize];
+        front = -1;
+        rear = -1;
+    }
+
+    public boolean isEmpty()
+    {
+        return (count == 0);
+    }
+
+    public boolean isFull()
+    {
+        return (count == maxSize);
+    }
+
+    public int getSize()
+    {
+        return count ;
+    }
+
+    /*  Function to check the front element of the queue */
+    public int queueFront()
+    {
+        if (count == 0)
+        {
+        	System.out.println("Queue is empty!");
+        	return -999;
+        }
+        else
+        {
+        	return queue[front];
+        }
+    }
+
+    /*  Function to check the rear element of the queue */
+    public int queueRear()
+    {
+        if (count == 0)
+        {
+        	System.out.println("Queue is empty!");
+        	return -999;
+        }
+        else
+        {
+        	return queue[rear];
+        }
+    }
+
+    public boolean enqueue(int dataIn)
+    {
+    	if(count == maxSize)
+    	{
+        	System.out.println("Queue is full!");
+    		return false;
+    	}
+    	
+    	rear = (rear + 1) % maxSize;
+    	queue[rear] = dataIn;
+        count++ ;
+        return true;
+    }
+    
+    /*  Function to remove front element from the queue. Time complexity O(1) */
+    public int dequeue()
+    {
+    	if(count == 0)
+    	{
+        	System.out.println("Queue is empty!");
+    		return -999;
+    	}
+    	
+    	front = (front + 1) % maxSize;
+    	int dataOut = queue[front];
+    	count --;
+    	return dataOut;
+    }
+    
+    public static void main(String[] args)
+    {
+        Queue queue = new Queue(10);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        
+        while ( !queue.isEmpty() )
+        {
+        	System.out.print( queue.dequeue() +" ");
+        }
+    }
+}
